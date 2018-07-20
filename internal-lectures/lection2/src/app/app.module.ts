c8346994403bfda4d2e7c94c2e8eed10d1b60a24
Daria Checherynda <daria.checherynda@softheme.com>
2018-07-20 15:50:43 +0300

    Lection 2: Data binding

---
+++ internal-lectures/lection2/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { OrdersComponent } from './orders/orders.component';
+import { OrderDetailsComponent } from './order-details/order-details.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        OrdersComponent,
+        OrderDetailsComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
